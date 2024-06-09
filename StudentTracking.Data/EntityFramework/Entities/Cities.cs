acd5ae0884ed46da95577d3101509c466279fd8c
ilhan_Kertmen <ilhan@kaizentech.net>
2024-06-09 22:14:50 +0300

    unnecessary parts removed

--- StudentTracking.Data/EntityFramework/Entities/Cities.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace StudentTracking.Data.EntityFramework.Entities
-{
-	public class Cities
-	{
-		public int CityID { get; set; }
-		public string City { get; set; }
-		public int Priority { get; set; }
-	}
-}
--- StudentTracking.Data/EntityFramework/StudentTrackingContext.cs
+++ StudentTracking.Data/EntityFramework/StudentTrackingContext.cs
@@ -13,8 +13,6 @@ namespace StudentTracking.Data.EntityFramework
             optionsBuilder.UseSqlServer("server=DESKTOP-748VRTV;database=StudentTracking; integrated security=true;TrustServerCertificate=True;");
         }
 
-        public DbSet<Customer> Customers { get; set; }
-        public DbSet<Cities> Cities { get; set; }
         public DbSet<User> Users { get; set; }
         public DbSet<UserType> UserTypes { get; set; }
         public DbSet<OutgoingMail> OutgoingMails { get; set; }
@@ -23,16 +21,8 @@ namespace StudentTracking.Data.EntityFramework
         public DbSet<Grade> Grades { get; set; }
         public DbSet<Absence> Absences { get; set; }
 
-
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Customer>().HasKey(p => p.CustomerID);
-            modelBuilder.Entity<Customer>().Property(p => p.Createdate).HasDefaultValue(DateTime.Now);
-            modelBuilder.Entity<Customer>().Property(p => p.Status).HasDefaultValue(0);
-
-            modelBuilder.Entity<Cities>().HasKey(p => p.CityID);
-            modelBuilder.Entity<Cities>().Property(p => p.Priority).HasDefaultValue(0);
-
             modelBuilder.Entity<User>().HasKey(p => p.Id);
             modelBuilder.Entity<User>().Property(p => p.CreatedDate).HasDefaultValue(DateTime.Now);
 
